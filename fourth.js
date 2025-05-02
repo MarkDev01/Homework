a93f943e5acdfec161f4b6b2016a27e17fa2bde5
MarkDev01 <MarkByNBA@gmail.com>
2025-05-02 12:35:07 +0300

    fourth

---
+++ fourth.js
@@ -0,0 +1,23 @@
+function multiplication(a, b) {
+  console.log(a * b)
+  let sum = a + b * 10
+  let mult = sum * a * b / 2
+  console.log(mult)
+}
+
+multiplication(23, 43)
+multiplication(233, 453)
+multiplication(233, 432)
+
+
+function sayWelcome(firstName, secondName) {
+  console.log(`Welcome, ${firstName} ${secondName}`)
+}
+
+sayWelcome('Mark', 'Komissachuk')
+
+let message = `LeBron James
+Kobe Braynt
+Mark Komissarchuk
+`
+console.log(message)
